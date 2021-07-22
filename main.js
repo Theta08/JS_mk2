ba355afcaefc26792714b654eade521c3b89bcd5
Theta08 <67591505+Theta08@users.noreply.github.com>
2021-07-22 15:37:56 +0900

    test

--- main.js
+++ main.js
@@ -2,4 +2,11 @@
 //'use strict'; ECMAScript5  오직 바닐라js만 쓴다.
 //성능 계선
 'use strict';
-console.log('hellow world!');
+const toggleBtn= document.querySelector('.navbar_toogleBtn');
+const menu= document.querySelector('.navbar_menu');
+const icons= document.querySelector('.navbar_icon');
+
+toggleBtn.addEventListener('click',()=>{
+    menu.classList.toggle('active');    //active가 있으면 뺴주고 없으면 엑티브 추가
+    icons.classList.toggle('active');
+})
